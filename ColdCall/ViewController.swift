744d9a2b886dc7292a9b1c3728bfe9bd74251445
grantwbrooks <grantwbrooks@gmail.com>
2017-09-07 15:44:53 -0400

    finished cold call

--- ColdCall/ViewController.swift
+++ ColdCall/ViewController.swift
@@ -9,15 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let nameBank = ["Jimmy","Cody","Grant","Harshada","Toro"]
+    
+    @IBOutlet weak var topLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func callButton(_ sender: Any) {
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        let rando = Int(arc4random_uniform(5))
+        topLabel.text = nameBank[rando]
     }
 
 

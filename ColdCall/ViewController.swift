caa5b2b9a86e6490e8a552ef6fe8f0a706bac4ed
grantwbrooks <grantwbrooks@gmail.com>
2017-09-07 15:49:11 -0400

    cleaned up

--- ColdCall/ViewController.swift
+++ ColdCall/ViewController.swift
@@ -20,21 +20,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
     
     @IBAction func callButton(_ sender: Any) {
-        
-        updateUI()
-    }
-    
-    func updateUI() {
         let rando = Int(arc4random_uniform(5))
         topLabel.text = nameBank[rando]
     }
-
-
+    
 }
 
